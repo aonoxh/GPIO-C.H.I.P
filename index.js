e17666bce5644576aa5b0dc675e580638d9f6a20
Hector Segura <aonoxh2@gmail.com>
2017-03-04 13:51:27 -0300

    .

--- index.js
+++ index.js
@@ -4,42 +4,41 @@ var exec = require('child_process').execSync;
 module.exports = {
 
 	IN : "in",
-	OUT : "out",
-
-	exportAll(){
-		for(var i = 0; i <= 7; i++){
-			exec('echo ' + i + " > /sys/class/gpio/export");
-		}
-	},
-
-	export(id){
-		exec('echo ' + id + " > /sys/class/gpio/export");
-	},
-
-	unexportAll(){
-		for(var i = 0; i <= 7; i++){
-			exec('echo ' + i + " > /sys/class/gpio/unexport");
-		}
-	},
-
-	unexport(id){
-		exec('echo ' + id + " > /sys/class/gpio/unexport");
-	},
-
-	read(id){
-		return exec('/sys/class/gpio' + id);
-	},
-
-	write(id, state){
-		if(state == 0 || state == 1){
-			exec('echo ' + state + ' > /sys/class/gpio' + id);
-		}
-	},
-
-	isChip(){
-		return (exec('cat /proc/meminfo').split()[0] > 380) ? true : false;
-	}
+	OUT : "out"
 }
+module.exports.exportAll = function(){
+	for(var i = 0; i <= 7; i++){
+		exec('echo ' + i + " > /sys/class/gpio/export");
+	}
+};
+
+module.exports.export = function(id){
+	exec('echo ' + id + " > /sys/class/gpio/export");
+};
+
+module.exports.unexportAll = function(){
+	for(var i = 0; i <= 7; i++){
+		exec('echo ' + i + " > /sys/class/gpio/unexport");
+	}
+};
+
+module.exports.unexport = function(id){
+	exec('echo ' + id + " > /sys/class/gpio/unexport");
+};
+
+module.exports.read = function(id){
+	return exec('/sys/class/gpio' + id);
+};
+
+module.exports.write = function(id, state){
+	if(state == 0 || state == 1){
+		exec('echo ' + state + ' > /sys/class/gpio' + id);
+	}
+};
+
+module.exports.isChip = function(){
+	return (exec('cat /proc/meminfo').split()[0] > 380) ? true : false;
+};
 
 var Gpio = class Gpio{
 	constructor(number, direction){
