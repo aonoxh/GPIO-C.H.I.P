3a8b785c7bf8e8981128ce8bc242b2d8e8c6245c
Hector Segura <aonoxh@users.noreply.github.com>
2017-08-08 18:49:25 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
 var os = require('os');
 var fs = require('fs');
 
-var MAP;
-
-if (os.release().startsWith("4.4.13-ntc-mlc")) {
-	MAP = [1013, 1014, 1015, 1016, 1017, 1018, 1019, 1020];
-} else if (os.release().startsWith("4.4")) {
-  	MAP = [1016, 1017, 1018, 1019, 1020, 1021, 1022, 1023];
-} else {
-  	MAP = [408, 409, 410, 411, 412, 413, 414, 415];
-}
-
 module.exports = {
 	IN : "in",
-	OUT : "out"
+	OUT : "out",
+	HIGH : "1",
+	LOW : "0",
+	RISING: "rising",
+	FALLING: "falling",
+	BOTH: "both",
+	NONE: "none"
 };
 
-module.exports.exportAll = function(){
+var PINS;
+
+if(os.release().startsWith("4.4.13-ntc-mlc")){
+	PINS = [1013, 1014, 1015, 1016, 1017, 1018, 1019, 1020];
+}else if(os.release().startsWith("4.4")){
+	PINS = [1016, 1017, 1018, 1019, 1020, 1021, 1022, 1023];
+}else{
+	PINS = [408, 409, 410, 411, 412, 413, 414, 415];
+}
+
+module.exports.export = (pin) => {
+	if(module.exports.isChip()){
+		if(!module.exports.isExported(pin)){
+			fs.writeFileSync("/sys/class/gpio/export", PINS[pin]);
+		}		
+	}
+}
+
+module.exports.exportAll = () => {
 	if(module.exports.isChip()){
 		for(var i = 0; i <= 7; i++){
 			if(!module.exports.isExported(i)){
@@ -24,68 +38,79 @@ module.exports.exportAll = function(){
 			}
 		}
 	}
-};
+}
 
-module.exports.export = function(id){
+module.exports.unexport = (pin) => {
 	if(module.exports.isChip()){
-		if(!module.exports.isExported(id)){
-			fs.writeFileSync("/sys/class/gpio/export", MAP[id]);
-		}		
-	}
-};
+		if(module.exports.isExported(pin)){
+			fs.writeFileSync("/sys/class/gpio/unexport", PINS[pin]);
+		}
+	}	
+}
 
-module.exports.unexportAll = function(){
+module.exports.unexportAll = () => {
 	if(module.exports.isChip()){
 		for(var i = 0; i <= 7; i++){
-			if(module.exports.isExported(i)){
-				module.exports.unexport(i);
-			}
+			module.exports.unexport(i);
 		}
-	}	
-};
-
-module.exports.unexport = function(id){
-	if(module.exports.isChip()){
-		if(!module.exports.isExported(id)){
-			fs.writeFileSync("/sys/class/gpio/unexport", MAP[id]);
-		}
-	}	
-};
-
-module.exports.direction = function(id, direction){
-	if(module.exports.isChip()){
-		if(!module.exports.isExported(id)){
-			module.exports.export(id);
-		}
-		fs.writeFileSync("/sys/class/gpio/gpio" + MAP[id] +"/direction", direction);
 	}
-};
+}
 
-module.exports.read = function(id){
+module.exports.direction = (pin, direction) => {
 	if(module.exports.isChip()){
-		if(!module.exports.isExported(id)){
-			module.exports.export(id);
+		if(module.exports.isExported(pin)){
+			fs.writeFileSync("/sys/class/gpio/gpio" + PINS[pin] +"/direction", direction);
 		}
-		return fs.readFileSync('/sys/class/gpio/gpio'+ MAP[id] + "/value").toString().replace("\n", "");
 	}
-};
+}
 
-module.exports.write = function(id, state, direction){
+module.exports.read = (pin) => {
 	if(module.exports.isChip()){
-		if(!module.exports.isExported(id)){
-			module.exports.export(id);
+		if(module.exports.isExported(pin)){
+			return fs.readFileSync('/sys/class/gpio/gpio'+ PINS[pin] + "/value").toString().replace("\n", "");
 		}
-		if(state == 0 || state == 1){
-			module.exports.direction(id, direction);
-			fs.writeFileSync('/sys/class/gpio/gpio' + MAP[id] + "/value", state);
-		}
-	}	
-};
+	}
+}
 
-module.exports.isExported = function(id){
+module.exports.write = (pin, value) => {
+	if(module.exports.isChip()){
+		if(module.exports.isExported(pin)){
+			module.exports.direction(pin, direction);
+			fs.writeFileSync('/sys/class/gpio/gpio' + PINS[pin] + "/value", value);
+		}
+	}
+}
+
+module.exports.edge = (pin, type, callback) => {
+	if(module.exports.isChip()){
+		if(module.exports.isExported(pin)){
+			
+		}
+	}
+}
+
+module.exports.on = (pin) => {
+	if(module.exports.isChip()){
+		if(module.exports.isExported(pin)){
+			module.exports.direction(pin, module.exports.OUT);
+			fs.writeFileSync('/sys/class/gpio/gpio' + PINS[pin] + "/value", module.exports.HIGH);
+		}		
+	}
+}
+
+module.exports.off = (pin) => {
+	if(module.exports.isChip()){
+		if(module.exports.isExported(pin)){
+			module.exports.direction(pin, module.exports.OUT);
+			fs.writeFileSync('/sys/class/gpio/gpio' + PINS[pin] + "/value", module.exports.LOW);
+		}
+	}
+}
+
+module.exports.isExported = (pin) => {
 	if (module.exports.isChip()) {
 		try{
-			fs.accessSync('/sys/class/gpio/gpio' + MAP[id], fs.R_OK | fs.W_OK);
+			fs.accessSync('/sys/class/gpio/gpio' + PINS[pin], fs.R_OK | fs.W_OK);
 		}catch(e){
 			return false;
 		}
@@ -94,26 +119,48 @@ module.exports.isExported = function(id){
 	}
 }
 
-module.exports.isChip = function(){
+module.exports.isChip = () => {
 	return (os.release().startsWith("4.4.13-ntc-mlc")) ? true : false;
-};
+}
 
+/* GPIO CLASS*/
 var Gpio = function Gpio(number, direction){
-	this.id = number;
+	this.pin = number;
 	this.direction = direction;
-	module.exports.export(this.id);
+	module.exports.export(this.pin);
 }
 
 Gpio.prototype.on = function(){
-	module.exports.write(this.id, 1, this.direction);
+	module.exports.write(this.pin, 1, this.direction);
 }
 
 Gpio.prototype.off = function(){
-	module.exports.write(this.id, 0, this.direction);
+	module.exports.write(this.pin, 0, this.direction);
 }
 
 Gpio.prototype.read = function(){
-	return module.exports.read(this.id);
+	return module.exports.read(this.pin);
 }
 
+Gpio.prototype.rising = (callback) => {
+
+}
+
+Gpio.prototype.falling = (callback) => {
+	
+};
+
 module.exports.Gpio = Gpio;
+/* GPIO CLASS END :C*/
+
+//SYNTAX EXAMPLES
+
+/*
+GPIO.edge(1,  (GPIO.BOTH | GPIO.RISING | GPIO.FALLING), (edge, value) => {
+	if(edge.equals(GPIO.RISING)){
+		console.log(edge, value);
+	}else{
+		console.log(edge, value);
+	}
+});
+*/
